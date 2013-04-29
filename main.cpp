be96f97adebd95c497faa07992ce35a9122f2a78
jam231 <=>
2013-04-29 23:56:50 +0200

    Przystosowalem do oblusgi managera konfiguracji.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <QApplication>
 
 #include "market.h"
+#include "configmanager.h"
 
-
+#include <QString>
 
 int main(int argv, char **args)
 {
 
+
     QApplication app(argv, args);
 
-    Market market;
+    // A bit ugly.
+    QString configFilePath = "Files/serverConfig.ini";
+    ConfigManager<> marketConfig(configFilePath);
+
+    Market market(marketConfig);
 
     return app.exec();
 }
