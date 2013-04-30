c99c51b64452478964473589b919390dbc2987fc
jam231 <mateusz.jany@gmail.com>
2013-04-30 22:07:59 +0200

    Miejsce na deklaracje i definicje nowych klas wyjatkow.

---
+++ exceptionhierarchy.cpp
@@ -0,0 +1,2 @@
+#include "exceptionhierarchy.h"
+
---
+++ exceptionhierarchy.h
@@ -0,0 +1,8 @@
+#ifndef EXCEPTIONHIERARCHY_H
+#define EXCEPTIONHIERARCHY_H
+
+#include <exception>
+
+class DummyException : public std::exception {};
+
+#endif // EXCEPTIONHIERARCHY_H
