15c208a516fff8800dc3f5bbf748cec673be3628
jam231 <mateusz.jany@gmail.com>
2013-04-30 22:01:08 +0200

    Dawniej pliki Utility.* .Narazie puste

---
+++ utilities.h
@@ -0,0 +1,34 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <QVector>
+#include <QPair>
+#include <QStringList>
+
+/*
+ *  It ought be lightweight, besides I really see
+ *  not point in having additional methods or fields.
+ */
+template<typename L, typename R>
+struct Either
+{
+    // Left means
+    bool isLeft;
+    L left;
+    R right;
+};
+
+template<typename T>
+struct Maybe
+{
+    Maybe() : isNothing(true), value(T()) {}
+    Maybe(T val) : isNothing(false), value(val) {}
+    bool isNothing;
+    T value;
+    operator bool() const
+    {
+        return !isNothing;
+    }
+};
+
+#endif // UTILITY_H
