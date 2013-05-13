b7718f53cbb129e69d678bb2e1b3dfd3bb5276f8
jam231 <mateusz.jany@gmail.com>
2013-05-13 03:36:17 +0200

    Plik .cpp ktory sie gdzies zapodzial w poprzednim commit.

---
+++ Messages/OMessages/registeruserrespok.cpp
@@ -0,0 +1,35 @@
+#include "registeruserrespok.h"
+
+#include <QDataStream>
+
+RegisterUserRespOk::RegisterUserRespOk(qint32 userId) : OMessage(),
+                                                          m_userId(userId)
+{
+}
+
+qint32 RegisterUserRespOk::length() const
+{ 
+    return sizeof(m_userId) + sizeof(type());
+}
+
+IOMessage::MessageType RegisterUserRespOk::type() const
+{
+    return REGISTER_USER_RESP_OK;
+}
+
+void RegisterUserRespOk::send(QIODevice* connection)
+{
+    // Domyślnie BigEndian
+    QDataStream out(connection);
+
+    // Nie wiem czemu trzeba tu robić cast'a
+    // IOMessage:MessageType to enum, który ma typ qint8
+    // jednak bez cast'a strumien traktuje type() jako 4 bajty.
+    out << static_cast<qint8>(type());
+    out << m_userId;
+}
+
+qint32 RegisterUserRespOk::getUserId() const
+{
+    return m_userId;
+}
