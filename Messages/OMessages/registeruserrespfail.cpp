7fed6af2e192aab0cfa360d391b93dd1ebbf376f
jam231 <mateusz.jany@gmail.com>
2013-05-27 13:25:52 +0200

    Patch.

--- Messages/OMessages/registeruserrespfail.cpp
+++ Messages/OMessages/registeruserrespfail.cpp
@@ -30,4 +30,5 @@ void RegisterUserRespFail::send(QIODevice* connection)
     out << static_cast<qint16>(sizeof(qint8) + sizeof(qint16) + reason.size())
         << static_cast<qint8>(type())
         << static_cast<qint16>(reason.size());
+    connection->write(m_reason.toUtf8());
 }
