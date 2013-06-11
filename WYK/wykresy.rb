3f834105aa5140f7a2d407c2bb8cf100260c7e43
kaiks <karol.lewcio@gmail.com>
2013-06-12 00:59:40 +0200

    Ładniejsze formatowanie wykresów

--- WYK/wykresy.rb
+++ WYK/wykresy.rb
@@ -4,17 +4,19 @@ set xdata time
 set timefmt \"%s\"
 # set xtics 3600
 set format x \"%H:%M:%S\"
+set style line 1 lt rgb \"#FF0000\" lw 2
 
-set terminal png size 1024,768
+set terminal pngcairo size 1024,768
+set lmargin 10
 set output 'out\\#{input}.png'
 set encoding iso_8859_2 
 set multiplot layout 2,1
 set size 1,0.8
 set origin 0,0.2
-plot '#{input}.txt' using 1:($2/100) with lines notitle
+plot '#{input}.txt' using 1:($2/100) with lines ls 1 notitle
 set size 1,0.2
 set origin 0,0.0
-#set ytics 10
+set ytics 50
 set boxwidth 0.5
 set style fill solid
 plot '#{input}.txt' using 1:3 with boxes notitle
