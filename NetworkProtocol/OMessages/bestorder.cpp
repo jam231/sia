26eca7d46d923eb395bf5ee9b0909796a026d41b
jam231 <mateusz.jany@gmail.com>
2013-09-26 01:06:11 +0200

    Issue#44 Wydaje sie teraz wszystko dzialac, ciekawe co powie travis.

--- NetworkProtocol/OMessages/bestorder.cpp
+++ NetworkProtocol/OMessages/bestorder.cpp
@@ -2,13 +2,12 @@
 
 namespace NetworkProtocol {
 
-
-BestOrderMsg::BestOrderMsg(Order::OrderType type, qint32 stockId, qint32 amount, qint32 price)
+BestOrderMsg::BestOrderMsg(DTO::Order::OrderType type, qint32 stockId, qint32 amount, qint32 price)
     : OrderMsg(type, stockId, amount, price)
 {
 }
 
-BestOrderMsg::BestOrderMsg(Order order)
+BestOrderMsg::BestOrderMsg(DTO::Order order)
     : OrderMsg(order)
 {
 }
