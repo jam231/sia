73bf20d28c1bec5e73002e3b7e3d849883aea79b
jam231 <mateusz.jany@gmail.com>
2014-03-02 13:38:14 +0100

    Przenioslem statyczne metody z Request do tych plikow, dodalem rowniez pare metod w tym fromStream, ktora, mam nadzieje, ulatwi zycie.

---
+++ NetworkProtocol/NetworkProtocol/networkprotocol_utilities.cpp
@@ -0,0 +1,126 @@
+#include "networkprotocol_utilities.h"
+
+namespace NetworkProtocol
+{
+namespace Requests
+{
+
+using namespace DTO;
+using namespace DTO::Types;
+using namespace DTO::Types::Message;
+
+std::shared_ptr<Request> fromStream(QDataStream& stream)
+{
+
+    Message::MessageLengthType length = readLength(stream);
+    Message::MessageType       type   = readType(stream);
+
+    // Read length bytes even if request is malformed.
+    QDataStream serialized_request(stream.device()->read(length));
+    Request* request;
+    switch(type)
+    {
+    case REQUEST_REGISTER_USER:
+        request = new RegisterUser(serialized_request);
+        break;
+    case REQUEST_LOGIN_USER:
+        request = new LoginUser(serialized_request);
+        break;
+    case REQUEST_SELL_STOCK_ORDER:
+        request = new SellStock(serialized_request);
+        break;
+    case REQUEST_BUY_STOCK_ORDER:
+        request = new BuyStock(serialized_request);
+        break;
+    case REQUEST_SUBSCRIBE_STOCK:
+        request = new SubscribeStock(serialized_request);
+        break;
+    case REQUEST_UNSUBSCRIBE_STOCK:
+        request = new UnsubscribeStock(serialized_request);
+        break;
+    case REQUEST_GET_MY_STOCKS:
+        request = new GetMyStocks(serialized_request);
+        break;
+    case REQUEST_GET_MY_ORDERS:
+        request = new GetMyOrders(serialized_request);
+        break;
+    case REQUEST_GET_STOCK_INFO:
+        request = new GetStockInfo(serialized_request);
+        break;
+    case REQUEST_CANCEL_ORDER:
+        request = new CancelOrder(serialized_request);
+        break;
+    case REQUEST_COMPANY_STATUS:
+        request = new CompanyStatus(serialized_request);
+        break;
+    //case REQUEST_SESSION_STATUS:
+    //    request = new SessionStatus(serialized_request);
+    //    break;
+    default:
+        LOG_ERROR(QString("Invalid type(%1). This shouldn't happpen.").arg(type));
+        throw InvalidRequestType();
+        break;
+    };
+    return std::shared_ptr<Request>(request);
+}
+
+Message::MessageLengthType getLength(QIODevice* data)
+{
+    if(data->bytesAvailable() < 2)
+    {
+        LOG_TRACE(QString("Not enough bytes in device. Should be %1 is %2")
+                  .arg(sizeof(Message::MessageLengthType)).arg(data->bytesAvailable()));
+        return -1;
+    }
+    Message::MessageLengthType message_length;
+
+    QDataStream in(data->peek(sizeof(Message::MessageLengthType)));
+
+    in >> message_length;
+
+    return message_length;
+}
+
+Message::MessageLengthType readLength(QDataStream& stream)
+{
+    Message::MessageLengthType request_length = getLength(stream.device());
+    if(request_length < stream.device()->bytesAvailable())
+    {
+        LOG_TRACE(QString("Request is incomplete. Request supposed length: %1"\
+                          " Available bytes in stream %2.")
+                  .arg(request_length).arg(stream.device()->bytesAvailable()));
+        throw IncompleteRequest(request_length);
+    }
+    stream.skipRawData(sizeof(Message::MessageLengthType));
+    return request_length;
+}
+
+Message::MessageType readType_NoEx(QDataStream& stream)
+{
+    if(stream.device()->bytesAvailable() < 1)
+    {
+        LOG_TRACE(QString("Can't read %1 byte(s) from empty stream.")
+                  .arg(sizeof(Message::MessageType)));
+
+        return Message::MessageType::MESSAGE_UNDEFINED;
+    }
+    Message::MessageType type;
+    stream >> type;
+
+    return type;
+}
+
+Message::MessageType readType(QDataStream& stream)
+{
+    Message::MessageType type = readType_NoEx(stream);
+    if(type == Message::MESSAGE_UNDEFINED)
+    {
+        LOG_TRACE(QString("Invalid request type."));
+        throw InvalidRequestType();
+    }
+
+    return type;
+}
+
+}
+}
---
+++ NetworkProtocol/NetworkProtocol/networkprotocol_utilities.h
@@ -0,0 +1,90 @@
+#ifndef NETWORKPROTOCOL_UTILITIES_H
+#define NETWORKPROTOCOL_UTILITIES_H
+
+#include "utilities.h"
+
+
+#include "Requests/cancelordermsg.h"
+#include "Requests/buystockmsg.h"
+#include "Requests/companystatusmsg.h"
+#include "Requests/getmyordersmsg.h"
+#include "Requests/getmystocksmsg.h"
+#include "Requests/getstockinfomsg.h"
+#include "Requests/loginusermsg.h"
+#include "Requests/registerusermsg.h"
+#include "Requests/sellstockmsg.h"
+#include "Requests/subscribestockmsg.h"
+#include "Requests/unsubscribestockmsg.h"
+
+#include <stdexcept>
+
+namespace NetworkProtocol
+{
+namespace Requests
+{
+
+class NETWORKPROTOCOLSHARED_EXPORT InvalidRequest : public std::runtime_error
+{
+public:
+    explicit InvalidRequest(const std::string& what_arg)
+        : std::runtime_error(what_arg)
+    {}
+};
+
+
+// Use when there is not enough bytes in stream to read request
+class NETWORKPROTOCOLSHARED_EXPORT IncompleteRequest : public InvalidRequest
+{
+public:
+    explicit IncompleteRequest(DTO::Types::Message::MessageLengthType request_length)
+        : InvalidRequest(QString("Stream has less than %1 bytes.")
+                         .arg(request_length).toStdString())
+    {}
+};
+
+// Use when there is not enough bytes in stream to read *body* of the request.
+class NETWORKPROTOCOLSHARED_EXPORT MalformedRequest : public InvalidRequest
+{
+public:
+    explicit MalformedRequest (const std::string& what_arg)
+        : InvalidRequest(what_arg)
+    {}
+};
+
+
+// Use when type == Message::UNDEFINED_MESSAGE
+class NETWORKPROTOCOLSHARED_EXPORT InvalidRequestType : public InvalidRequest
+{
+public:
+    explicit InvalidRequestType()
+        : InvalidRequest("Invalid request type.")
+    {}
+};
+// Use when body contents are well formed, but violate data constrains
+class NETWORKPROTOCOLSHARED_EXPORT InvalidRequestBody : public InvalidRequest
+{
+public:
+    explicit InvalidRequestBody (const std::string& what_arg)
+        : InvalidRequest(what_arg)
+    {}
+};
+
+
+
+
+NETWORKPROTOCOLSHARED_EXPORT std::shared_ptr<Request> FromStream(QDataStream&);
+
+NETWORKPROTOCOLSHARED_EXPORT DTO::Types::Message::MessageLengthType getLength(QIODevice*);
+// Gets request type and discards sizeof(Message::MessageLengthType) bytes from stream.
+NETWORKPROTOCOLSHARED_EXPORT DTO::Types::Message::MessageLengthType readLength(QDataStream&);
+// Gets request type and discards sizeof(Message::MessageType) bytes from stream.
+// If there isn't enough bytes in the stream it returns MESSAGE_UNDEFINED.
+NETWORKPROTOCOLSHARED_EXPORT DTO::Types::Message::MessageType readType_NoEx(QDataStream&);
+// Does the same as readType, but throw InvalidRequestType on either MESSAGE_UNDEFINED
+// or not enough bytes.
+NETWORKPROTOCOLSHARED_EXPORT DTO::Types::Message::MessageType readType(QDataStream&);
+
+
+}
+}
+#endif // NETWORKPROTOCOL_UTILITIES_H
