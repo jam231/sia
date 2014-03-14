707e53d1988adecdf61c0de0b9ce33d9fa407bfb
jam231 <mateusz.jany@gmail.com>
2014-03-14 18:32:34 +0100

    Issue #47 - testy dla SubscribeStock.

--- NetworkProtocol/NetworkProtocol_Test/Requests/subscribestockmsg_test.h
+++ NetworkProtocol/NetworkProtocol_Test/Requests/subscribestockmsg_test.h
@@ -1,6 +1,29 @@
 #ifndef SUBSCRIBESTOCKMSG_TEST_H
 #define SUBSCRIBESTOCKMSG_TEST_H
 
-#include "../NetworkProtocol/Requests/subscribestockmsg.h"
+#include <QtTest>
+
+
+/*
+ *  Testing against specification in subscribemsg.h
+ */
+class SubscribeStockTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+
+    void initTestCase();
+
+    void from_valid_data();
+    void from_valid();
+
+    void from_invalid_body_data();
+    void from_invalid_body();
+
+
+    void from_malformed_data();
+    void from_malformed();
+};
 
 #endif // SUBSCRIBESTOCKMSG_TEST_H
