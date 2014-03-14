bb06ef9097bf8a207af8f9f9144039547a9d3d50
jam231 <mateusz.jany@gmail.com>
2014-03-14 15:13:38 +0100

    Issue #48 - Poprawki dla Requests::BuyStock.

--- NetworkProtocol/NetworkProtocol/Requests/buystockmsg.cpp
+++ NetworkProtocol/NetworkProtocol/Requests/buystockmsg.cpp
@@ -18,26 +18,27 @@ BuyStock::BuyStock(QDataStream& serialized_request)
 BuyStock::BuyStock(std::shared_ptr<AbstractLogger> logger,
                    QDataStream& serialized_request)
 {   
-    serialized_request >> _stockId;
-    serialized_request >> _amount;
-    serialized_request >> _price;
+
     if(serialized_request.device()->bytesAvailable() != sizeof(_stockId) +
                                                         sizeof(_amount) + sizeof(_price))
     {
         LOG_TRACE(logger,
                   QString("Malformed request: Not enough bytes in serialized_request"\
-                          " to read stock id, amount and price. Is %1 should be >%2.")
+                          " to read stock id, amount and price. Is %1 should be %2.")
                   .arg(serialized_request.device()->bytesAvailable())
                   .arg(sizeof(_stockId) + sizeof(_amount) + sizeof(_price)));
 
         throw MalformedRequest("Wrong number bytes in serialized_request for"\
-                               " stock id.");
+                               " stock id, amount and price.");
     }
+    serialized_request >> _stockId;
+    serialized_request >> _amount;
+    serialized_request >> _price;
     if(_stockId <= 0 || _amount <= 0 || _price <= 0)
     {
         LOG_TRACE(logger,
                   QString("Invalid request body: stockId(%1) <= 0 || amount(%2) <= 0 "\
-                          "|| price <= 0.")
+                          "|| price(%3) <= 0.")
                   .arg(_stockId.value).arg(_amount.value).arg(_price.value));
 
         throw InvalidRequestBody("One of stockId, amount, price is <= 0.");
