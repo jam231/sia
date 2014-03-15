54d47f47d1cac83f4083a382cab6d74bd6cf31de
jam231 <mateusz.jany@gmail.com>
2014-03-15 16:58:22 +0100

    Issue #48 - Okazuje sie, ze jezeli chce sie uzyc shared pointera po przekazaniu go innej funkcji/metodzie/czemus to nie mozna go przekazywac przez std::move. Wychodzi moja nieznajomosc cpp.

--- NetworkProtocol/NetworkProtocol/networkprotocol_utilities.cpp
+++ NetworkProtocol/NetworkProtocol/networkprotocol_utilities.cpp
@@ -1,5 +1,7 @@
 #include "networkprotocol_utilities.h"
 
+#include <assert.h>
+
 namespace NetworkProtocol
 {
 namespace Requests
@@ -8,6 +10,7 @@ namespace Requests
 using namespace DTO;
 using namespace DTO::Types;
 using namespace DTO::Types::Message;
+
 std::shared_ptr<Request> fromStream(QDataStream& stream)
 {
     return fromStream(std::move(GlobalUtilities::getLogger()), stream);
@@ -17,44 +20,58 @@ std::shared_ptr<Request> fromStream(std::shared_ptr<AbstractLogger> logger,
                                     QDataStream& stream)
 {
 
-    Message::MessageLengthType length = readLength(std::move(logger), stream);
-    Message::MessageType       type   = readType(std::move(logger), stream);
+    Message::MessageLengthType length = readLength(logger, stream);
+    if(length > stream.device()->bytesAvailable())
+    {
+        LOG_TRACE(logger,
+                  QString("Request is incomplete. Request supposed length: %1"\
+                          " Available bytes in stream %2.")
+                  .arg(length)
+                  .arg(stream.device()->bytesAvailable()));
+        throw IncompleteRequest(length);
+    }
+    // Message is at leat 3 bytes long, if it's shorter then discard it.
+    if(length < 3)
+    {
+        throw MalformedRequest("Length < 3");
+    }
+    Message::MessageType       type   = readType(logger, stream);
 
     // Read length bytes even if request is malformed.
-    QDataStream serialized_request(stream.device()->read(length));
+    QDataStream serialized_request_body(stream.device()->read(length));
     Request* request;
     try{
     switch(type)
     {
     case REQUEST_REGISTER_USER:
-        request = new RegisterUser(std::move(logger), serialized_request);
+        request = new RegisterUser(std::move(logger), serialized_request_body);
         break;
     case REQUEST_LOGIN_USER:
-        request = new LoginUser(std::move(logger), serialized_request);
+        request = new LoginUser(std::move(logger), serialized_request_body);
         break;
     case REQUEST_SELL_STOCK_ORDER:
-        request = new SellStock(std::move(logger), serialized_request);
+        request = new SellStock(std::move(logger), serialized_request_body);
         break;
     case REQUEST_BUY_STOCK_ORDER:
-        request = new BuyStock(std::move(logger), serialized_request);
+        request = new BuyStock(std::move(logger), serialized_request_body);
         break;
     case REQUEST_SUBSCRIBE_STOCK:
-        request = new SubscribeStock(std::move(logger), serialized_request);
+        request = new SubscribeStock(std::move(logger), serialized_request_body);
         break;
     case REQUEST_UNSUBSCRIBE_STOCK:
-        request = new UnsubscribeStock(std::move(logger), serialized_request);
+        request = new UnsubscribeStock(std::move(logger), serialized_request_body);
         break;
     case REQUEST_GET_MY_STOCKS:
-        request = new GetMyStocks(std::move(logger), serialized_request);
+        request = new GetMyStocks(std::move(logger), serialized_request_body);
         break;
     case REQUEST_GET_MY_ORDERS:
-        request = new GetMyOrders(std::move(logger), serialized_request);
+        request = new GetMyOrders(std::move(logger), serialized_request_body);
         break;
     case REQUEST_GET_STOCK_INFO:
-        request = new GetStockInfo(std::move(logger), serialized_request);
+        request = new GetStockInfo(std::move(logger), serialized_request_body);
         break;
     case REQUEST_CANCEL_ORDER:
-        request = new CancelOrder(std::move(logger), serialized_request);
+        request = new CancelOrder(std::move(logger), serialized_request_body);
         break;
     //case REQUEST_COMPANY_STATUS:
     //    request = new CompanyStatus(serialized_request);
@@ -79,7 +96,7 @@ std::shared_ptr<Request> fromStream(std::shared_ptr<AbstractLogger> logger,
 
 Message::MessageLengthType getLength(QIODevice* data)
 {
-    return getLength(std::move(GlobalUtilities::getLogger()), data);
+    return getLength(GlobalUtilities::getLogger(), data);
 }
 
 Message::MessageLengthType getLength(std::shared_ptr<AbstractLogger> logger,
@@ -91,7 +108,7 @@ Message::MessageLengthType getLength(std::shared_ptr<AbstractLogger> logger,
                   QString("Not enough bytes in device. Should be %1 is %2")
                   .arg(sizeof(Message::MessageLengthType))
                   .arg(data->bytesAvailable()));
-        return -1;
+        return 0;
     }
     Message::MessageLengthType message_length;
 
@@ -110,17 +127,19 @@ Message::MessageLengthType readLength(QDataStream& stream)
 Message::MessageLengthType readLength(std::shared_ptr<AbstractLogger> logger,
                                       QDataStream& stream)
 {
-    Message::MessageLengthType request_length = getLength(stream.device());
-    if(request_length > stream.device()->bytesAvailable())
+    Message::MessageLengthType request_length = getLength(logger,
+                                                          stream.device());
+    if(request_length == 0)
     {
-        LOG_TRACE(logger,
-                  QString("Request is incomplete. Request supposed length: %1"\
+       LOG_TRACE(logger,
+                  QString("Request is incomplete. Request should be at least %1 bytes"
                           " Available bytes in stream %2.")
-                  .arg(request_length)
+                  .arg(sizeof(request_length))
                   .arg(stream.device()->bytesAvailable()));
-        throw IncompleteRequest(request_length);
+        throw IncompleteRequest(sizeof(request_length));
     }
-    stream.skipRawData(sizeof(Message::MessageLengthType));
+
+    stream.skipRawData(sizeof(request_length));
     return request_length;
 }
 
@@ -154,7 +173,7 @@ Message::MessageType readType(QDataStream& stream)
 Message::MessageType readType(std::shared_ptr<AbstractLogger> logger,
                               QDataStream& stream)
 {
-    Message::MessageType type = readType_NoEx(stream);
+    Message::MessageType type = readType_NoEx(logger, stream);
     if(type == Message::MESSAGE_UNDEFINED)
     {
         LOG_TRACE(logger,
