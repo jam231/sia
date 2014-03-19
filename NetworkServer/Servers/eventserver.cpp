4f537816c2865e1790d27b7cbe8fb17045d37926
jam231 <mateusz.jany@gmail.com>
2014-03-19 19:41:22 +0100

    Event Server handles events - as of now, only market session maintenance. It is quite important to run it as standalone thread with high priority - Issue #51.

---
+++ NetworkServer/Servers/eventserver.cpp
@@ -0,0 +1,25 @@
+#include "eventserver.h"
+
+
+using namespace std;
+
+
+EventServer::EventServer(shared_ptr<AbstractDataStorageFactory> dataFactory,
+                         shared_ptr<AbstractLoggerFactory> loggerFactory)
+    : _dataFactory(move(dataFactory)), _loggerFactory(move(loggerFactory))
+{
+    auto logger = _loggerFactory->createLoggingSession();
+
+    LOG_TRACE(logger, "Setting up timers for events.");
+
+    // Set timers
+}
+
+void EventServer::run()
+
+{
+    LOG_INFO(_loggerFactory->createLoggingSession(),
+             "Starting new Event Server.");
+    _eventLoop.exec();
+}
+
---
+++ NetworkServer/Servers/eventserver.h
@@ -0,0 +1,44 @@
+#ifndef EVENTSERVER_H
+#define EVENTSERVER_H
+
+#include <QObject>
+#include <QRunnable>
+#include <QEventLoop>
+
+#include <../Utilities/logger.h>
+#include <DataStorage/datastorage.h>
+
+#include <memory>
+
+/*
+ * Event server handles market events, e.g. session starting/stoping
+ *
+ *
+ *
+ *
+ *
+ */
+
+/// STUB
+class EventServer : public QObject, public QRunnable
+{
+    QEventLoop _eventLoop;
+
+    std::shared_ptr<AbstractDataStorageFactory> _dataFactory;
+    std::shared_ptr<AbstractLoggerFactory> _loggerFactory;
+
+
+    Q_OBJECT
+public:
+    explicit EventServer(std::shared_ptr<AbstractDataStorageFactory> dataFactory,
+                         std::shared_ptr<AbstractLoggerFactory> loggerFactory);
+
+    void run();
+
+signals:
+
+public slots:
+
+};
+
+#endif // EVENTSERVER_H
