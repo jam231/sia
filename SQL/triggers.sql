7fb9ed0365fe32721224a19f155475b740497ff8
jam231 <mateusz.jany@gmail.com>
2014-04-12 01:33:32 +0200

    Issue #69 - Translated triggers.sql to english.

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -3,7 +3,7 @@
 
 CREATE OR REPLACE FUNCTION buy_order_on_delete() RETURNS TRIGGER AS $$
 BEGIN
-	--ZWROT KASY
+	-- return money to the buyer
 	UPDATE owned_stock SET amount=amount+old.amount*old.limit1 WHERE user_id=old.user_id AND stock_id=1;
 	RETURN old;
 END
@@ -13,42 +13,41 @@ CREATE TRIGGER bo_on_delete BEFORE DELETE on buy_order
 	FOR EACH ROW EXECUTE PROCEDURE buy_order_on_delete();
 
 	
-CREATE OR REPLACE FUNCTION zlecenie_sprzedazy_on_delete() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION sell_order_on_delete() RETURNS TRIGGER AS $$
 BEGIN
-	--ZWROT AKCJI
+	--return shares to the seller
 	UPDATE owned_stock SET amount=amount+old.amount WHERE user_id=old.user_id AND stock_id=old.stock_id;
 	RETURN old;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER zs_on_delete BEFORE DELETE on sell_order
-	FOR EACH ROW EXECUTE PROCEDURE zlecenie_sprzedazy_on_delete();
+CREATE TRIGGER so_on_delete BEFORE DELETE on sell_order
+	FOR EACH ROW EXECUTE PROCEDURE sell_order_on_delete();
 	
 --				 INSERT TRIGGERS
 
-CREATE OR REPLACE FUNCTION zlecenie_kupna_on_insert() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION buy_order_on_insert() RETURNS TRIGGER AS $$
 BEGIN
-	--wstawianie do tabeli posiadanych dobr juz stocku w amounti 0. bazuja na tym update'y w przypadku realizacji zlecenia
-	INSERT INTO owned_stock(user_id,stock_id,amount) --pewnie nalezaloby to w przyszlosci wywalic
+	-- some functions for managing order processing (helper_procedures.sql) rely on buyer owning stock he is buying  
+	INSERT INTO owned_stock(user_id,stock_id,amount)
     SELECT new.user_id,new.stock_id,0
 		WHERE NOT EXISTS (SELECT 1 FROM owned_stock WHERE user_id=new.user_id AND stock_id=new.stock_id);
 	   
 	UPDATE owned_stock SET amount=amount-new.amount*new.limit1 WHERE user_id=new.user_id AND stock_id=1;
 	
-	
-	IF (SELECT mozna_handlowac FROM stock WHERE stock_id=new.stock_id) THEN
-		PERFORM wykonaj_zlecenie_kupna(new);
+	IF (SELECT active FROM stock WHERE stock_id=new.stock_id) THEN
+		PERFORM wykonaj_buy_order(new);
 	END IF;
 	
 	RETURN new;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER zk_on_insert AFTER INSERT on buy_order
-	FOR EACH ROW EXECUTE PROCEDURE zlecenie_kupna_on_insert();
+CREATE TRIGGER bo_on_insert AFTER INSERT on buy_order
+	FOR EACH ROW EXECUTE PROCEDURE buy_order_on_insert();
 
 	
-CREATE OR REPLACE FUNCTION zlecenie_sprzedazy_on_insert() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION sell_order_on_insert() RETURNS TRIGGER AS $$
 BEGIN
 	UPDATE owned_stock SET amount=amount-new.amount WHERE user_id=new.user_id AND stock_id=new.stock_id;
 	
@@ -60,38 +59,38 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER zs_on_insert AFTER INSERT on sell_order
-	FOR EACH ROW EXECUTE PROCEDURE zlecenie_sprzedazy_on_insert();
+CREATE TRIGGER so_on_insert AFTER INSERT on sell_order
+	FOR EACH ROW EXECUTE PROCEDURE sell_order_on_insert();
 	
 	
-CREATE OR REPLACE FUNCTION zrealizowane_zlecenie_on_insert() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION transaction_on_insert() RETURNS TRIGGER AS $$
 BEGIN
 	PERFORM pg_notify('ch_last_transaction_change',new.stock_id||'|'||new.amount||'|'||new.price||'|'||new.time);
 	RETURN new;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER zz_on_insert AFTER INSERT on transaction
-	FOR EACH ROW EXECUTE PROCEDURE zrealizowane_zlecenie_on_insert();
+CREATE TRIGGER tr_on_insert AFTER INSERT on transaction
+	FOR EACH ROW EXECUTE PROCEDURE transaction_on_insert();
 	
 --				 UPDATE TRIGGERS
 
-CREATE OR REPLACE FUNCTION zlecenie_kupna_on_update() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION buy_order_on_update() RETURNS TRIGGER AS $$
 BEGIN
-	IF new.amount=1 THEN --jesli zlecenie jest zrealizowane to sie usuwa
+	IF new.amount=0 THEN --if order is completed, i.e. have amount = 0 then delete it from buy_order table
 		DELETE FROM buy_order WHERE order_id=new.order_id;
 	END IF;
 	RETURN new;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER zk_on_update AFTER UPDATE ON buy_order
-	FOR EACH ROW EXECUTE PROCEDURE zlecenie_kupna_on_update();
+CREATE TRIGGER bo_on_update AFTER UPDATE ON buy_order
+	FOR EACH ROW EXECUTE PROCEDURE buy_order_on_update();
 
 		
-CREATE OR REPLACE FUNCTION zlecenie_sprzedazy_on_update() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION sell_order_on_update() RETURNS TRIGGER AS $$
 BEGIN
-	IF new.amount=1 THEN --jesli zlecenie jest zrealizowane to sie usuwa
+	IF new.amount=0 THEN --if order is completed, i.e. have amount = 0 then delete it from buy_order table
 		DELETE FROM sell_order WHERE order_id=new.order_id;
 	END IF;
 		
@@ -99,6 +98,6 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER zs_on_update AFTER UPDATE ON sell_order
-	FOR EACH ROW EXECUTE PROCEDURE zlecenie_sprzedazy_on_update();
+CREATE TRIGGER so_on_update AFTER UPDATE ON sell_order
+	FOR EACH ROW EXECUTE PROCEDURE sell_order_on_update();
 	
